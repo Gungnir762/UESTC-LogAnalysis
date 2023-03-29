d3be153b4c73633b9ac0f188588da889741377c0
龙雀 <83220033+Gungnir762@users.noreply.github.com>
2023-03-29 19:42:01 +0800

    update

--- cronConfig.py
+++ cronConfig.py
@@ -34,7 +34,8 @@ if __name__ == '__main__':
         # 测试用
         # job = cron.new(command=rf'echo "hello world,`date`" >> {program_path}cron.log')
         job = cron.new(
-            command=rf'python3 {program_path}updateDB.py -p {program_path} >> {program_path}cron.log 2>&1 </dev/null &')
+            command=rf'python3 {program_path}updateDB.py -p {program_path} -l {log_path} \
+            >> {program_path}cron.log 2>&1 </dev/null &')
         job.minute.every(cron_interval)
         print("restarting cron service")
     subprocess.call("sudo service cron restart", shell=True)
